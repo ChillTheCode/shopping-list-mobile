45df6d73dd7b1e23a0006cd0b42c3af5ef36c70d
ChillTheCode <edwin.daniel@ui.ac.id>
2023-11-22 16:27:57 +0700

    N

--- lib/widgets/shop_card.dart
+++ lib/widgets/shop_card.dart
@@ -52,10 +52,12 @@ class ShopCard extends StatelessWidget {
             MaterialPageRoute(builder: (context) => const ProductPage()));
       }
 
-      else if (item.name == "Logout") {
+  
+// statement if sebelumnya
+// tambahkan else if baru seperti di bawah ini
+else if (item.name == "Logout") {
         final response = await request.logout(
-            // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
-            "http://127.0.0.1:8000/auth/logout/");
+            "http://http://127.0.0.1:8000/auth/logout/");
         String message = response["message"];
         if (response['status']) {
           String uname = response["username"];
@@ -72,6 +74,7 @@ class ShopCard extends StatelessWidget {
           ));
         }
       }
+
 
   },
         child: Container(
